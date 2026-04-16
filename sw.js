58405d1543c41618e838d944f9ae995015fb32c1
pezzaliapp <pezzalialessandro@gmail.com>
2026-04-16 21:17:55 +0200

    remove debug bar fix pagina bianca v1.5.4

--- sw.js
+++ sw.js
@@ -8,7 +8,7 @@
  *   notifica l'utente e skippa il waiting automaticamente
  */
 
-const APP_VERSION = 'djapp-v1.5.3'
+const APP_VERSION = 'djapp-v1.5.4'
 const CACHE_NAME = `${APP_VERSION}`
 
 // Asset da pre-cachare (vengono aggiornati ad ogni build)
